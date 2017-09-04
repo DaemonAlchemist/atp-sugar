9e6538ed4c8a7d741813221d38e0768f1b56a7c4
Andrea <andrea@wittrock.us>
2017-09-03 21:24:19 -0500

    Add additional tests

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@
  */
 
 import assert from 'assert';
-import {o} from 'atp-sugar';
+import {o, mergeFlags} from 'atp-sugar';
 
 describe('ATP-Sugar', () => {
     describe('Object', () => {
@@ -78,6 +78,68 @@ describe('ATP-Sugar', () => {
                 );
             });
         });
+        describe("#merge", () => {
+           it('should merge recursively by default', () => {
+               assert.deepEqual(
+                   o({foo: 1, bar: {baz: 2}}).merge({bar: {fizz: 3}}).raw,
+                   {foo: 1, bar: {baz: 2, fizz: 3}}
+               );
+           });
 
+            it('should concatenate arrays by default', () => {
+                assert.deepEqual(
+                    o({foo: 1, bar: [1, 2]}).merge({bar: [3, 4]}).raw,
+                    {foo: 1, bar: [1, 2, 3, 4]}
+                );
+            });
+
+            it('should overwrite objects if not merging recursively', () => {
+                assert.deepEqual(
+                    o({foo: 1, bar: {baz: 2}}).merge({bar: {fizz: 3}}, mergeFlags.CONCAT_ARRAYS).raw,
+                    {foo: 1, bar: {fizz: 3}}
+                );
+            });
+
+            it('should overwrite arrays if not concatenating them', () => {
+                assert.deepEqual(
+                    o({foo: 1, bar: [1, 2]}).merge({bar: [3, 4]}, mergeFlags.RECURSIVE).raw,
+                    {foo: 1, bar: [3, 4]}
+                );
+            });
+        });
+        describe("#reduce", () => {
+            it('should work like array.reduce', () => {
+               assert.equal(
+                   o({foo: 1, bar: 2, baz: 3, fizz: 4}).reduce((total, num) => total + num, 0),
+                   10
+               );
+            });
+            it('should work without an initial value', () => {
+                assert.equal(
+                    o({foo: 1, bar: 2, baz: 3, fizz: 4}).reduce((total, num) => total + num),
+                    10
+                );
+            })
+        });
+        describe.skip('#mergeReduce', () => {
+            //TODO:  mergeReduce tests
+            it('should have tests', () => {});
+        });
+        describe.skip('#switch', () => {
+            //TODO:  switch tests
+            it('should have tests', () => {});
+        });
+        describe.skip('#delete', () => {
+            //TODO:  delete tests
+            it('should have tests', () => {});
+        });
+        describe.skip('#values', () => {
+            //TODO:  values tests
+            it('should have tests', () => {});
+        });
+    });
+    describe.skip('Array', () => {
+        //TODO:  array tests
+        it('should have tests', () => {});
     });
 });
