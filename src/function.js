8cf44a478667966de1839e0e4dc5fb5c50136ed0
Andrew Wittrock <andy@wittrock.us>
2017-09-18 13:16:18 -0500

    Add function delay

---
+++ src/function.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Andy on 4/12/2017.
+ */
+
+import {AtpObject, o} from './object';
+
+class DelayedEvent {
+    constructor(callback) {
+        this.callback = callback;
+        this.event = null;
+        this.delay = 0;
+    }
+
+    cancel() {
+        clearTimeout(this.event);
+        return this;
+    }
+
+    runIn(delay, save = true) {
+        if(save) this.delay = delay;
+        this.cancel();
+        this.event = setTimeout(this.callback, delay);
+        return this;
+    }
+
+    milliseconds() {return this;}
+    seconds() {this.runIn(this.delay           * 60 * 1000, false); return this;}
+    minutes() {this.runIn(this.delay           * 60 * 1000, false); return this;}
+    hours()   {this.runIn(this.delay      * 60 * 60 * 1000, false); return this;}
+    days()    {this.runIn(this.delay * 24 * 60 * 60 * 1000, false); return this;}
+}
+
+export class AtpFunction extends AtpObject
+{
+    delay() {
+        return new DelayedEvent(this.raw);
+    }
+}
+
+export const f = func => func instanceof AtpFunction ? func : new AtpFunction(func);
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
 
 import {a} from './array';
 import {o, mergeFlags} from './object';
+import {f} from "./function";
 import {collate} from './collate';
 
-export {o, a, mergeFlags, collate};
+export {o, a, f, mergeFlags, collate};
